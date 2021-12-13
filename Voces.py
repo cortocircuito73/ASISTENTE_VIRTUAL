e67b05e5b9292ca32bb946816dd741137888698b
cortocircuito73 <90864761+cortocircuito73@users.noreply.github.com>
2021-12-13 18:05:58 -0500

    Update Voces.py

--- Voces.py
+++ Voces.py
@@ -2,13 +2,8 @@ import pyttsx3, time
 
 tts=pyttsx3.init()
 voices=tts.getProperty('voices')
-tts.setProperty('voice',voices[1].id)
-tts.say('suscribete a corto circuito 73')
-tts.say('no olvides dejar tu like, comentar y compartirlo con tus amigos')
-tts.runAndWait()
 
-
-'''i=0
+i=0
 for voice in voices:
 	print(voice)
 	tts.setProperty('voice',voices[i].id)
@@ -16,4 +11,3 @@ for voice in voices:
 	tts.runAndWait()
 	time.sleep(1)
 	i+=1
-	'''
